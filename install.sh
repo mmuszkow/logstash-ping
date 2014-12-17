bd6e7da864968f86bb33d698e7b12edbeed0ea23
Maciek Muszkowski <maciek.muszkowski@gmail.com>
2014-12-17 12:22:00 +0100

    ping_group_range privileges

--- install.sh
+++ install.sh
@@ -2,12 +2,13 @@
 mkdir -p /opt/lib/
 gcc -Wall -O2 -fpic -shared -o /opt/lib/libtinyping.so tinyping.c \
     || echo "Cannot install libtinyping.so" && exit 1
+echo "0 2147483647" > /proc/sys/net/ipv4/ping_group_range
 LS_DIR=""
 for dir in /usr/local/logstash /opt/logstash; do
     [ -d "$dir" ] && LS_DIR="$dir" && break
 done
 if [ "x$LS_DIR" != "x" ]; then
-    cp ping.rb $LS_DIR/lib/logstash/inputs/ && echo "Installed!"
+    cp ping.rb $LS_DIR/lib/logstash/inputs/ && echo "Installed! Please restart logstash"
 else
     echo "Cannot determine Logstash location" && exit 2
 fi
